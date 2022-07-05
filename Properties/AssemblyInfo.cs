b8cd0b3ba2b16c4e7e79067547ca77591764cd88
Chris Hanson <developer@wirescience.com>
2022-07-05 14:11:33 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("WireScience.Data.Database.Auth")]
+[assembly: AssemblyDescription("aspnet.core identity database")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WireScience, Inc.")]
+[assembly: AssemblyProduct("WireScience.Data.Database.Auth")]
+[assembly: AssemblyCopyright("©2022 WireScience")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
