88370f2dc92fefc9c97373fd7bf8d613b6c8fdd0
Chris Hanson <developer@wirescience.com>
2022-07-26 23:42:56 +0200

    users | add confirm email procedure
    https://github.com/WireScience/maxamoto-content/issues/26

---
+++ dbo/Stored Procedures/User_ConfirmEmail.sql
@@ -0,0 +1,16 @@
+﻿create procedure dbo.User_ConfirmEmail
+(
+  @email  nvarchar(256)
+)
+as
+begin
+
+  update dbo.AspNetUsers
+  set
+    EmailConfirmed = 1
+  where
+    Email = @email
+
+  return 0;
+
+end
